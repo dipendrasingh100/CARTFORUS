8f71f39c0955eb14e498f44c21011c2b23cb423b
dipendrasingh100 <dipsingh9950@gmail.com>
2023-10-16 15:39:38 +0530

    Loader added in Cart.js

--- client/src/host.js
+++ client/src/host.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 // https://carforus.onrender.com
-const server = axios.create({ baseURL: "https://carforus.onrender.com" })
+const server = axios.create({ baseURL: "http://localhost:8000" })
 export default server
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -6,6 +6,7 @@ import { handleLink } from '../utils/helperFuction'
 import "../css/cart.css"
 // import PaypalCheckoutButton from '../components/PaypalCheckoutButton'
 // import completion from "../assets/success.gif"
+import Loader from "../components/Loader"
 import emptycart from "../assets/emptycart.gif"
 
 const Cart = () => {
@@ -14,7 +15,7 @@ const Cart = () => {
   const location = useLocation()
 
   // const [success, setSuccess] = useState(false)
-  const { user, error } = useSelector(state => state.user)
+  const { user, error, isLoading } = useSelector(state => state.user)
 
   useEffect(() => {
     handleLink()
@@ -51,75 +52,76 @@ const Cart = () => {
   }
 
   return (
-    <>
-      <div className='p-main-cont'>
+    <>{
+      isLoading ? <Loader /> :
         <div className='p-main-cont'>
-          <div className="p-sub-cont">
-            <div className="cart-left">
-              <h2>Your Shopping Cart</h2>
-              {user.cart.length === 0 ?
-                <div className="gif">
-                  <img src={emptycart} alt="emptycart" />
-                </div>
-                : user.cart.map(item => (
-                  <div className="p-cont" key={item._id}>
-                    <div className="img-cont">
-                      <img src={item.productId.images && item.productId?.images[0]} alt="item" />
-                    </div>
-                    <div className="mid">
-                      <Link to={`/product/${item.productId?._id}`}>
-                        <span>{item.productId.title}</span><br />
-                      </Link>
-                      <div className="p-price-cont cart">
-                        <div className="dis-p">₹{item.productId?.dis_price}</div>
-                        <div className="act-p">₹{item.productId?.act_price}</div>
-                        <div className="discount">{item.productId?.discount}% OFF</div>
-                      </div>
-                      <div className="p-botton">
-                        <div className="addtocart">
-                          <button className='btn' onClick={() => decreaseCartQuantity(item._id)} disabled={item.quantity === 1}>-</button>
-                          <input type="number" readOnly value={item.quantity} />
-                          <button className='btn' onClick={() => increaseCartQuantity(item._id)}>+</button>
-                        </div>
-                        <span onClick={() => removeItem(item._id)}>Remove</span>
-                      </div>
-                    </div>
+          <div className='p-main-cont'>
+            <div className="p-sub-cont">
+              <div className="cart-left">
+                <h2>Your Shopping Cart</h2>
+                {user.cart.length === 0 ?
+                  <div className="gif">
+                    <img src={emptycart} alt="emptycart" />
                   </div>
-                ))}
-            </div>
-            <div className="cart-right">
-              <h3>Price Details</h3>
-              <div>
-                <div className="items">
-                  <span>Price ({user.cart.length} item)</span>
-                  <span>₹{findPrice()}</span>
-                </div>
-                <div className="items">
-                  <span>Shipping Fee</span>
-                  <span>Free</span>
-                </div>
-                <div className="items">
-                  <span>Tax</span>
-                  <span>₹0</span>
-                </div>
-                <div className="total">
-                  <span>Total Amount</span>
-                  <span>₹{findPrice()}</span>
-                </div>
+                  : user.cart.map(item => (
+                    <div className="p-cont" key={item._id}>
+                      <div className="img-cont">
+                        <img src={item.productId.images && item.productId?.images[0]} alt="item" />
+                      </div>
+                      <div className="mid">
+                        <Link to={`/product/${item.productId?._id}`}>
+                          <span>{item.productId.title}</span><br />
+                        </Link>
+                        <div className="p-price-cont cart">
+                          <div className="dis-p">₹{item.productId?.dis_price}</div>
+                          <div className="act-p">₹{item.productId?.act_price}</div>
+                          <div className="discount">{item.productId?.discount}% OFF</div>
+                        </div>
+                        <div className="p-botton">
+                          <div className="addtocart">
+                            <button className='btn' onClick={() => decreaseCartQuantity(item._id)} disabled={item.quantity === 1}>-</button>
+                            <input type="number" readOnly value={item.quantity} />
+                            <button className='btn' onClick={() => increaseCartQuantity(item._id)}>+</button>
+                          </div>
+                          <span onClick={() => removeItem(item._id)}>Remove</span>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
               </div>
-              {user.cart.length > 0 &&
-                <button className='btn place-order' onClick={handleOrder}>Place Order</button>
-              }
-              {/* 
+              <div className="cart-right">
+                <h3>Price Details</h3>
+                <div>
+                  <div className="items">
+                    <span>Price ({user.cart.length} item)</span>
+                    <span>₹{findPrice()}</span>
+                  </div>
+                  <div className="items">
+                    <span>Shipping Fee</span>
+                    <span>Free</span>
+                  </div>
+                  <div className="items">
+                    <span>Tax</span>
+                    <span>₹0</span>
+                  </div>
+                  <div className="total">
+                    <span>Total Amount</span>
+                    <span>₹{findPrice()}</span>
+                  </div>
+                </div>
+                {user.cart.length > 0 &&
+                  <button className='btn place-order' onClick={handleOrder}>Place Order</button>
+                }
+                {/* 
                   <hr />
 
                   <div className="paypal-button-cont">
                     {user?.cart.length > 0 && <PaypalCheckoutButton total={findPrice()} setSuccess={setSuccess} />}
                   </div> */}
+              </div>
             </div>
-          </div>
-        </div >
-      </div >
+          </div >
+        </div >}
     </>
   )
 }
